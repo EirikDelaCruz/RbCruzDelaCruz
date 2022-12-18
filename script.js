16b40eb61b78bc18282a1a93c662c8f951e122d8
MiloGabrielCruz <112594469+MiloGabrielCruz@users.noreply.github.com>
2022-12-18 21:43:01 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+let done = "false";
+
 function chbg(color) {
-    document.getElementById('backgroundFade').style.backgroundColor = color;
-}   
+  document.getElementById('backgroundFade').style.backgroundColor = color;
+}
+
+
+function Socmed() {
+
+  if (done == "false") {
+    document.getElementById("Socmed").style.marginRight = '130px';
+    done = "true";
+    document.getElementById("hamburger").style.transform = 'rotate(90deg)'
+  }
+  else {
+
+    document.getElementById("Socmed").style.marginRight = '-100px';
+    done = "false"
+    document.getElementById("hamburger").style.transform = 'rotate(0deg)'
+  }
+}
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
