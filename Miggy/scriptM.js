fa4f614ae8b47b5823db41f946aeab702140f33a
MiloGabrielCruz <112594469+MiloGabrielCruz@users.noreply.github.com>
2023-01-22 19:44:39 +0800

    Add files via upload

---
+++ Miggy/scriptM.js
@@ -0,0 +1,424 @@
+let done1 = "false";
+let done2 = "false";
+let done3 = "false";
+let done4 = "false";
+let done5 = "false";
+let done6 = "false";
+let done7 = "false";
+let done8 = "false";
+let done9 = "false";
+let done10 = "false";
+let done11 = "false";
+let done12 = "false";
+let done13 = "false";
+let done14 = "false";
+let done15 = "false";
+let done16 = "false";
+
+function Zoom1() {
+
+  if (done1 == "false") {
+    document.getElementById("BIND1").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND1").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND1").style.zIndex = '4';
+    done1 = "true";
+
+  }
+}
+
+function Zoom1a() {
+
+  if (done1 == "true") {
+    document.getElementById("BIND1").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND1").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND1").style.zIndex = '1';
+    done1 = "false"
+
+  }
+}
+
+function Zoom1() {
+
+  if (done1 == "false") {
+    document.getElementById("BIND1").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND1").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND1").style.zIndex = '4';
+    done1 = "true";
+
+  }
+}
+
+function Zoom1a() {
+
+  if (done1 == "true") {
+    document.getElementById("BIND1").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND1").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND1").style.zIndex = '1';
+    done1 = "false"
+
+  }
+}
+
+function Zoom2() {
+
+  if (done1 == "false") {
+    document.getElementById("BIND2").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND2").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND2").style.zIndex = '4';
+    done2 = "true";
+
+  }
+}
+
+function Zoom2a() {
+
+  if (done2 == "true") {
+    document.getElementById("BIND2").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND2").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND2").style.zIndex = '1';
+    done2 = "false"
+
+  }
+}
+
+function Zoom3() {
+
+  if (done3 == "false") {
+    document.getElementById("BIND3").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND3").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND3").style.zIndex = '4';
+    done3 = "true";
+
+  }
+}
+
+function Zoom3a() {
+
+  if (done3 == "true") {
+    document.getElementById("BIND3").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND3").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND3").style.zIndex = '1';
+    done3 = "false"
+
+  }
+}
+
+function Zoom4() {
+
+  if (done4 == "false") {
+    document.getElementById("BIND4").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND4").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND4").style.zIndex = '4';
+    done4 = "true";
+
+  }
+}
+
+function Zoom4a() {
+
+  if (done4 == "true") {
+    document.getElementById("BIND4").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND4").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND4").style.zIndex = '1';
+    done4 = "false"
+
+  }
+}
+
+function Zoom5() {
+
+  if (done5 == "false") {
+    document.getElementById("BIND5").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND5").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND5").style.zIndex = '4';
+    done5 = "true";
+
+  }
+}
+
+function Zoom5a() {
+
+  if (done5 == "true") {
+    document.getElementById("BIND5").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND5").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND5").style.zIndex = '1';
+    done5 = "false"
+
+  }
+}
+
+function Zoom6() {
+
+  if (done6 == "false") {
+    document.getElementById("BIND6").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND6").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND6").style.zIndex = '4';
+    done6 = "true";
+
+  }
+}
+
+function Zoom6a() {
+
+  if (done6 == "true") {
+    document.getElementById("BIND6").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND6").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND6").style.zIndex = '1';
+    done6 = "false"
+
+  }
+}
+
+function Zoom7() {
+
+  if (done7 == "false") {
+    document.getElementById("BIND7").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND7").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND7").style.zIndex = '4';
+    done7 = "true";
+
+  }
+}
+
+function Zoom7a() {
+
+  if (done7 == "true") {
+    document.getElementById("BIND7").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND7").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND7").style.zIndex = '1';
+    done7 = "false"
+
+  }
+}
+
+function Zoom8() {
+
+  if (done8 == "false") {
+    document.getElementById("BIND8").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND8").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND8").style.zIndex = '4';
+    done8 = "true";
+
+  }
+}
+
+function Zoom8a() {
+
+  if (done8 == "true") {
+    document.getElementById("BIND8").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND8").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND8").style.zIndex = '1';
+    done8 = "false"
+
+  }
+}
+
+function Zoom9() {
+
+  if (done9 == "false") {
+    document.getElementById("BIND9").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND9").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND9").style.zIndex = '4';
+    done9 = "true";
+
+  }
+}
+
+function Zoom9a() {
+
+  if (done9 == "true") {
+    document.getElementById("BIND9").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND9").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND9").style.zIndex = '1';
+    done9 = "false"
+
+  }
+}
+
+function Zoom10() {
+
+  if (done10 == "false") {
+    document.getElementById("BIND10").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND10").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND10").style.zIndex = '4';
+    done10 = "true";
+
+  }
+}
+
+function Zoom10a() {
+
+  if (done10 == "true") {
+    document.getElementById("BIND10").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND10").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND10").style.zIndex = '1';
+    done10 = "false"
+
+  }
+}
+
+function Zoom11() {
+
+  if (done11 == "false") {
+    document.getElementById("BIND11").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND11").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND11").style.zIndex = '4';
+    done11 = "true";
+
+  }
+}
+
+function Zoom11a() {
+
+  if (done11 == "true") {
+    document.getElementById("BIND11").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND11").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND11").style.zIndex = '1';
+    done11 = "false"
+
+  }
+}
+
+function Zoom12() {
+
+  if (done12 == "false") {
+    document.getElementById("BIND12").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND12").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND12").style.zIndex = '4';
+    done12= "true";
+
+  }
+}
+
+function Zoom12a() {
+
+  if (done12 == "true") {
+    document.getElementById("BIND12").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND12").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND12").style.zIndex = '1';
+    done12 = "false"
+
+  }
+}
+
+function Zoom13() {
+
+  if (done13 == "false") {
+    document.getElementById("BIND13").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND13").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND13").style.zIndex = '4';
+    done13 = "true";
+
+  }
+}
+
+function Zoom13a() {
+
+  if (done13 == "true") {
+    document.getElementById("BIND13").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND13").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND13").style.zIndex = '1';
+    done13 = "false"
+
+  }
+}
+
+function Zoom14() {
+
+  if (done14 == "false") {
+    document.getElementById("BIND14").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND14").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND14").style.zIndex = '4';
+    done14 = "true";
+
+  }
+}
+
+function Zoom14a() {
+
+  if (done14 == "true") {
+    document.getElementById("BIND14").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND14").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND14").style.zIndex = '1';
+    done14 = "false"
+
+  }
+}
+
+function Zoom15() {
+
+  if (done15 == "false") {
+    document.getElementById("BIND15").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND15").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND15").style.zIndex = '4';
+    done15 = "true";
+
+  }
+}
+
+function Zoom15a() {
+
+  if (done15 == "true") {
+    document.getElementById("BIND15").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND15").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND15").style.zIndex = '1';
+    done15 = "false"
+
+  }
+}
+
+function Zoom16() {
+
+  if (done16 == "false") {
+    document.getElementById("BIND16").style.transform = 'scale(1.5) translateX(400px)';
+    document.getElementById("BIND16").style.opacity = '100';
+    document.getElementById("Bind100").style.filter = 'blur(10px)'
+    document.getElementById("BIND16").style.zIndex = '4';
+    done16 = "true";
+
+  }
+}
+
+function Zoom16a() {
+
+  if (done16 == "true") {
+    document.getElementById("BIND16").style.transform = 'translateX(-400px)';
+    document.getElementById("BIND16").style.opacity = '0';
+    document.getElementById("Bind100").style.filter = 'blur(0)'
+    document.getElementById("BIND16").style.zIndex = '1';
+    done16 = "false"
+
+  }
+}
